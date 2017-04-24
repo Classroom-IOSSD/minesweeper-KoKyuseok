fd335649f8bb08a481666726ce1faf02b17b089a
KoKyuseok <onine94@hanmail.net>
2017-04-24 22:39:02 +0900

    Refactored conio.c

--- conio.c
+++ conio.c
@@ -3,7 +3,7 @@
 static struct termios old, new;
 
 /* Initialize new terminal i/o settings */
-void initTermios(int echo) {
+void initTerminal(int echo) {
     tcgetattr(0, &old); /* grab old terminal i/o settings */
     new = old; /* make new settings same as old settings */
     new.c_lflag &= ~ICANON; /* disable buffered i/o */
@@ -12,14 +12,14 @@ void initTermios(int echo) {
 }
 
 /* Restore old terminal i/o settings */
-void resetTermios(void) {
+void resetTerminal(void) {
     tcsetattr(0, TCSANOW, &old);
 }
 
 /* Read 1 character - echo defines echo mode */
 char getch_(int echo) {
     char ch;
-    initTermios(echo);
+    initTerminal(echo);
     ch = getchar();
     if(ch == '\x1b') {
         if(getchar()=='[') {
@@ -39,7 +39,7 @@ char getch_(int echo) {
             }
         }
     }
-    resetTermios();
+    resetTerminal();
     return ch;
 }
 
