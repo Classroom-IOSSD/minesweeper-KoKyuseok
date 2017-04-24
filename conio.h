52f57c7387448025a0e69886d9a7251ebed2c6b7
KoKyuseok <onine94@hanmail.net>
2017-04-24 22:39:16 +0900

    Refactored conio.h

--- conio.h
+++ conio.h
@@ -1,10 +1,10 @@
 #include <termios.h>
 #include <stdio.h>
 
-void initTermios(int echo);
+void initTerminal(int echo);
 
 /* Restore old terminal i/o settings */
-void resetTermios(void);
+void resetTerminal(void);
 
 /* Read 1 character - echo defines echo mode */
 char getch_(int echo);
